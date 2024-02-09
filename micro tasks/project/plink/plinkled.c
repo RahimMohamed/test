fee9b831cd5eb256844e45d7c2cee1d6893ae746
RahimMohamed <111053544+RahimMohamed@users.noreply.github.com>
2024-02-09 14:03:29 +0200

    new

---
+++ micro tasks/project/plink/plinkled.c
@@ -0,0 +1,23 @@
+#include <REGX52.h>
+delay (unsigned int y)
+{
+      unsigned int i,j;
+      for (i-0;i<y;i++)
+	    {
+           for (j=0;j<1275;j++)
+            {
+						}
+			}
+			return 0;
+		}
+
+main ()
+{
+ while(1)
+   {
+      delay(100);
+      P1 = 0XFF;
+      delay(100);
+      P1 = 0X00;
+    }
+	}
